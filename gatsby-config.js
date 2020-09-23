c1cd8bcaa3103655318b0960e530d4468f09c1a9
Oliver Smit <olmesm@gmail.com>
2020-09-23 07:24:39 +0100

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,8 +77,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
   ],
 }
